60b069a94653ebb370f652bfa676768763e45040
carti6666 <platform.admin@hmrkr.info>
2025-09-08 18:38:07 +0900

    1515

--- js/main.js
+++ js/main.js
@@ -1,28 +1,51 @@
 fetch("../data/data.json")
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
   .then((data) => {
     const mainSection = document.querySelector("main section");
     mainSection.innerHTML = "";
 
     data.albums.forEach((album) => {
-      const albumGridHTML = `
-        <div class="grid-container">
-          <div class="grid-item">
-            <p><span class="bold-text"><i>${album.number}</i></span></p>
-          </div>
-          <div class="grid-item">
-            <span class="bold-text">${album.artist}</span> - ${album.title}
-          </div>
-          <div class="grid-item"></div>
-          <div class="grid-item">${album.format} | ${album.tracks} | ${album.genre}</div>
-          <div class="grid-item"></div>
-          <div class="grid-item">${album.date}</div>
-          <div class="grid-item merged-column">
-            <img src="${album.image}" alt="${album.title} 앨범 커버">
-          </div>
-        </div>
+      const albumTableHTML = `
+        <table class="album-table">
+          <tbody>
+            <tr>
+              <td class="td-1">
+                <p><span class="bold-text"><i>${album.number}</i></span></p>
+              </td>
+              <td class="td-2"><span class="bold-text">${album.artist}</span> - ${album.title}</td>
+              <td rowspan="3" class="td-3" >
+                <a href="${album.links.buy}">
+                  <img class="cover-img" src="${album.image}" alt="${album.title}">
+                </a>
+              </td>
+              <td rowspan="3" class="td-4">
+                <img class="ol-img" src="assets/images/arrow-trend-up-svgrepo-com.svg">
+              </td>
+            </tr>
+            <tr>
+              <td><a href="${album.links.listen}">listen</a></td>
+              <td>${album.format} | ${album.tracks} | ${album.genre}</td>
+              <td class="td-4"></td>
+            </tr>
+            <tr>
+              <td><a href="${album.links.buy}">buy</a></td>
+              <td>${album.date}</td>
+              <td class="td-4"></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td class="td-4"></td>
+            </tr>
+          </tbody>
+        </table>
       `;
-      mainSection.innerHTML += albumGridHTML;
+      mainSection.innerHTML += albumTableHTML;
     });
   })
   .catch((error) => {
